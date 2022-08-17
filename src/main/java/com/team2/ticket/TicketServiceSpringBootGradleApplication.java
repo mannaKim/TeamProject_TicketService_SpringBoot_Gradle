a67d79a5b6a60f2a8a221b3952d1ed5d695a3c48
mannaKim <gkssk2286@naver.com>
2022-08-17 14:29:24 +0900

    Initial commit

---
+++ src/main/java/com/team2/ticket/TicketServiceSpringBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.team2.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceSpringBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketServiceSpringBootGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team2/ticket/TicketServiceSpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceSpringBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
